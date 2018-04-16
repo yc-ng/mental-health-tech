8ae9fe4c68b5a1a8196cdbc17d5eca70d8f4ed67
yc-ng <mizzerische@yahoo.com>
2018-04-16 23:32:26 +0800

    Add contingency tables to the Shiny dashboard

--- shinytest/app.R
+++ shinytest/app.R
@@ -13,28 +13,30 @@ ui <- fluidPage(
         column(width = 4,
                selectInput("col_1", 
                            label = "Variable to display",
-                           choices = names(survey_factors))
-               ),
+                           choices = names(survey_factors))),
         column(width = 3, offset = 1,
                selectInput("twocol_1",
                            label = "Variable on x-axis",
-                           choices = names(survey_factors))
-               ),
+                           choices = names(survey_factors))),
         column(width = 3,
                selectInput("twocol_2",
                            label = "Variable by colour",
-                           choices = names(survey_factors))
-               )
+                           choices = names(survey_factors)))
     ),
     
     fluidRow(
         column(width = 4,
-               plotOutput("barplot")
-               ),
+               plotOutput("barplot")),
         
         column(width = 6, offset = 1,
-               plotOutput("mosaicplot")
-               )
+               plotOutput("mosaicplot"))
+    ),
+    
+    fluidRow(
+      column(width = 3, offset = 1,
+             tableOutput("table1d")),
+      column(width = 6, offset = 2,
+             tableOutput("table2d"))
     )
 )
 
@@ -48,6 +50,18 @@ server <- function(input, output){
     output$mosaicplot <- renderPlot(
         ggplot(survey, aes_string(input$twocol_1)) +
             geom_bar(aes_string(fill = input$twocol_2))
+    )
+    
+    output$table1d <- renderTable(
+        table(survey[[input$col_1]])
+    )
+    
+    output$table2d <- renderTable({
+        table(survey[[input$twocol_1]],
+              survey[[input$twocol_2]]) %>% 
+            addmargins() %>%
+            as.data.frame.matrix()},
+        include.rownames = TRUE
     )
 }
 
